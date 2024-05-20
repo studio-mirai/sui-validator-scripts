1bbdf2254383f48a054ba2a5ce6cdc57a2550f7d
Brian Li <brian@brianli.com>
2024-05-20 22:08:10 +0900

    Optimize merge coins script.

--- scripts/mergeSuiCoins.sh
+++ scripts/mergeSuiCoins.sh
@@ -2,21 +2,14 @@
 #
 # Merge SUI coins to a single coin.
 
-# Create a list of all Sui coins.
-# Output array starts at index 1 ([1:]) because index 0 is gas coin.
-suiCoins=$(sui client gas --json | jq -r '.[1:] | .[].gasCoinId')
-
-# Convert Sui coins into an array
-suiCoinArray=($suiCoins)
+# Create a list of all Sui coins excluding the gas coin (index 0) and format them for merging.
+formattedSuiCoins=$(sui client gas --json | jq -r '.[1:] | .[].gasCoinId' | sed 's/^/@/; s/ /, @/g')
 
 # Check if exactly one Sui coin is found
-if [ ${#suiCoinArray[@]} -eq 1 ]; then
+if [ -z "$formattedSuiCoins" ]; then
     echo "Only one Sui coin found. Exiting script."
     exit 0
 fi
-
-# Format Sui coins for merging
-formattedSuiCoins=$(echo $suiCoins | sed 's/^/@/; s/ /, @/g')
 
 # Merge coins into a single Sui coin.
 sui client ptb \
